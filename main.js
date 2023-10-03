6f796abafd67d0e0f85199aab5da8381b251e2b5
33Quentin <138811269+33Quentin@users.noreply.github.com>
2023-10-03 17:37:03 +0200

    Update main.js
    import * as THREE from './node_modules/three/build/three.module.js';

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css';
-import * as THREE from 'three';
+
+import * as THREE from './node_modules/three/build/three.module.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 //import { FontLoader } from 'three';
