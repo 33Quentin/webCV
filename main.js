f30f38936e1f23d0c9c1bc312e6e4f6972fecaa4
33Quentin <138811269+33Quentin@users.noreply.github.com>
2023-10-04 13:20:45 +0200

    Update main.js
    remove fontloader

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 import './style.css';
-
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
-//import { FontLoader } from 'three';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 import TextTexture from '@seregpie/three.text-texture';
 
